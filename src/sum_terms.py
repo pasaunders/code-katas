41de9e4f876877a49b036f5e21eb7b66ecc11c04
pasaunders <kaorti@gmail.com>
2017-02-20 12:06:43 -0800

    sum terms kata now python2 compatable

--- src/sum_terms.py
+++ src/sum_terms.py
@@ -1,5 +1,7 @@
 """Generate a list of n items, sum it and round it to two decimal places."""
 
+from __future__ import division
+
 
 def series_sum(n):
     """Generate sum and round to two decimal places a list of n items."""
