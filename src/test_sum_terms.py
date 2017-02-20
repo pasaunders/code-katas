b9858a2bf8d1ee18c5db5e81a31ea8de1985c730
pasaunders <kaorti@gmail.com>
2017-02-20 12:02:37 -0800

    updated additional kata tests

--- src/test_sum_terms.py
+++ src/test_sum_terms.py
@@ -1,3 +1,17 @@
-Test.assert_equals(series_sum(1), "1.00")
-Test.assert_equals(series_sum(2), "1.25")
-Test.assert_equals(series_sum(3), "1.39")
+"""Convert codewars kata tests to pytest."""
+
+import pytest
+
+output_table = [
+    [1, "1.00"],
+    [2, "1.25"],
+    [3, "1.39"]
+]
+
+
+@pytest.mark.parametrize('input, output', output_table)
+def test_series(input, output):
+    """Test that series_sum outputs according to the expected formula."""
+    from sum_terms import series_sum
+    print(input)
+    assert series_sum(input) == output
