d2f1ae2c012cccdef29e145ad54930e49589713f
pasaunders <kaorti@gmail.com>
2017-02-20 10:01:54 -0800

    setup autocomplete assignment

--- src/autocomplete.py
+++ src/autocomplete.py
@@ -1,3 +1,10 @@
 """Attempt at creating an autocomplete class."""
 
-class autocomplete
+
+class Autocomplete(object):
+    """Takes a vocab list on init., provides methods to autocomplete."""
+
+    def __init__(self, vocab, max_comletions=5):
+        """Initialize autocomplete."""
+        self.vocab = vocab
+
