d129a45901199990f6860b9b733ea79aed4a3f0d
pasaunders <kaorti@gmail.com>
2017-04-06 10:53:35 -0700

    refactor for simplicity

--- src/kebabize.py
+++ src/kebabize.py
@@ -3,13 +3,13 @@
 
 def kebabize(string):
     new_string = ''
-    for idx, item in enumerate(string):
+    for item in string:
         if item.isupper():
-            if len(new_string) > 0 and string[idx - 1] is not '-':
+            if len(new_string) > 0 and new_string[-1] is not '-':
                 new_string += '-'
             new_string += item.lower()
         elif item.islower():
             new_string += item
-        elif item is '-' and len(new_string) > 0 and string[idx - 1] is not '-':
+        elif item is '-' and len(new_string) > 0 and new_string[-1] is not '-':
             new_string += '-'
     return new_string
