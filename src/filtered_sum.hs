d027034b1f93ffa1fb11cca3ff162d3b152004af
pasaunders <kaorti@gmail.com>
2017-06-22 17:56:53 -0700

    added better solution from codewars answers

--- src/filtered_sum.hs
+++ src/filtered_sum.hs
@@ -5,3 +5,7 @@ import Data.List
 sumArray :: Maybe [Int] -> Int
 sumArray Nothing = 0
 sumArray (Just xs) = sum (drop 1 (take (length xs - 1) (sort xs)))
+
+sumArray2 :: Maybe [Int] -> Int
+sumArray2 (Just xs@(_:_:_)) = sum xs - maximum xs - minimum xs
+sumArray2 _ = 0
