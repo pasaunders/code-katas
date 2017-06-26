21aff606535a31c23690e43c45f95c048fe98ac6
pasaunders <kaorti@gmail.com>
2017-06-26 12:06:22 -0700

    much more elegant solution

--- src/descending.hs
+++ src/descending.hs
@@ -4,7 +4,11 @@ import Data.Char
 import Data.List
 
 descendingOrder :: Integer -> Integer
-descendingOrder x = read (map intToDigit (sortBy (flip compare) (makeDigits x))) :: Integer
+descendingOrder x = read (map intToDigit (sortBy (flip compare) (makeDigits x)))
 
 makeDigits :: Integer -> [Int]
 makeDigits = map digitToInt . show
+
+
+descendingOrder2 :: Integer -> Integer
+descendingOrder2 = read . reverse . sort . show
