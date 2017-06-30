ba7c3c70e45acb37cc7d1a7d82859db1dcb95660
pasaunders <kaorti@gmail.com>
2017-06-30 10:18:09 -0700

    improved readability and compactness from codewars answers, lesson to remember where notation and use more of the builtins

--- src/count_digit.hs
+++ src/count_digit.hs
@@ -7,3 +7,7 @@ nbDig n d = foldr (+) 0 (map  (\a -> stringDigits (show (a^2)) d) [0..n])
 
 stringDigits :: String -> Int -> Int
 stringDigits num digit = length (filter (== intToDigit digit) num)
+
+nbDig2 :: Int -> Int -> Int
+nbDig2 n d = sum [length $ filter (==c) $ show (x*x) | x <- [0..n]]
+  where c = intToDigit d
